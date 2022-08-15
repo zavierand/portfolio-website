5cdb6853a33b9b58d72960f73a342ac110165bb4
zavierand <93751988+zavierand@users.noreply.github.com>
2022-08-15 10:35:48 -0400

    Update mobile navbar in Navbar.jsx
    fixed bug in the mobile navbar that messed up sizing.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
         </div>
 
         {/* mobile menu */}
-        <ul className={!nav ? 'hidden' : 'abosule top-0 left-0 w-full h-screen bg-[#2C2F33] flex flex-col justify-center items-center'}>
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#2C2F33] flex flex-col justify-center items-center'}>
             <li className='py-6 text-4xl'>
                 <Link onClick={handleClick} to="home" smooth={true} duration={500}>
                 home
