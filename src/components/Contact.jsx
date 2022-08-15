4be6546971076edced3fd04075fbd574040c7478
zavierand <93751988+zavierand@users.noreply.github.com>
2022-08-15 10:56:36 -0400

    Update Contact.jsx
    fixed email address

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method="POST" action="https://getform.io/f/47797dc6-2760-4061-9df7-6e0c7e5d08c4" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#C252E1] text-[#2A2356]'>Contact</p>
-                <p className='text-[#2A2356] py-4'>reach out - zavierand098@gmail.com !!</p>
+                <p className='text-[#2A2356] py-4'>reach out - zavierand98@gmail.com !!</p>
             </div>
             <input className='p-2 bg-[#CCD6F6]' type='text' placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#CCD6F6]' type='email' placeholder='Email' name='email' />
